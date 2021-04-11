38629d771820ada575ff9d75ba498f54cc329d83
NiladriG21 <78343082+NiladriG21@users.noreply.github.com>
2021-04-11 13:32:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,80 @@
-var fixedRect,movingRect;
-function setup() {
-  createCanvas(800,400);
-  fixedRect= createSprite(200, 200, 50, 80);
-  movingRect= createSprite(400, 200, 80, 30);
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
+var divisons = [];
+var divisonsHeight = 300;
+var particles = [];
+var plinkos = []; 
+function preload()
+{
+	
 }
+
+
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(400,690,800,20);
+
+  
+  for (var i = 0; i<=width; i= i+80) {
+      divisons.push(new Division(i,height-divisonsHeight/2, 10, divisonsHeight));
+  }
+
+  for (var j=75; j<=width; j = j+50) {
+    plinkos.push(new Plinko(j, 75));
+  }
+  for (var j=50; j<=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,175));
+  }
+  for (var j=75; j<=width; j=j+50) {
+    plinkos.push(new Plinko(j,275));
+  }
+  for (var j=50; j<=width-10; j=j+50) {
+    plinkos.push(new Plinko(j,375));
+  }
+
+  
+
+ 
+	Engine.run(engine);
+  
+}
+
 
 function draw() {
-  background(255,255,255);  
-  movingRect.x=World.mouseX;
-  movingRect.y=World.mouseY;
-if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 )
-  {movingRect.shapeColor="red";
-  fixedRect.shapeColor="red";
-
-}
-else{movingRect.shapeColor="green";
-fixedRect.shapeColor="green";
-
-}
+  rectMode(CENTER);
+  background(0);
+  
   drawSprites();
+
+  
+  
+  for (var n = 0; n<divisons.length; n++) {
+    divisons[n].display();
+  }
+
+  if (frameCount %60 === 0) {
+    particles.push(new Particle(random(width/2-30, width/2+30),10,10));
+  }
+  
+  for (var h = 0; h<particles.length; h++) {
+    particles[h].display();
+  }
+
+  for (var j = 0; j<plinkos.length; j++) {
+    plinkos[j].display();
+  }
+
+
+  ground.display();
+ 
+
+ 
 }
